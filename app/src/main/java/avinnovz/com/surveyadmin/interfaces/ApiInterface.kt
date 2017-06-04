00c1c5229e0ebafa79d01e77d96675d5ad53064b
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-06-04 08:45:27 +0800

    Mapped api request to fetch my profile

--- app/src/main/java/avinnovz/com/surveyadmin/interfaces/ApiInterface.kt
+++ app/src/main/java/avinnovz/com/surveyadmin/interfaces/ApiInterface.kt
@@ -1,6 +1,7 @@
 package proto.com.kotlinapp.interfaces
 
 import avinnovz.com.surveyadmin.models.request.Login
+import avinnovz.com.surveyadmin.models.response.MyProfile
 import proto.com.kotlinapp.models.response.LoginResponse
 import retrofit2.http.*
 import rx.Observable
@@ -13,4 +14,7 @@ interface ApiInterface {
     @POST("/api/user/auth")
     @Headers("Content-Type: application/json")
     fun login(@Body login: Login): Observable<LoginResponse>
+
+    @GET("/api/users/me")
+    fun myProfile(@Header("Authorization") header: String): Observable<MyProfile>
 }
